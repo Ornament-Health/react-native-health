98c9e9570e7d4279556c3d78021a65caca20b03a
Anastasia Mishur <40164951+mobileapps23@users.noreply.github.com>
2024-08-01 15:12:59 +0300

    bump example project

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,13 +5,52 @@
  * @format
  */
 
-module.exports = {
+const path = require('path');
+const escape = require('escape-string-regexp');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const pak = require('../package.json');
+
+const root = path.resolve(__dirname, '..');
+
+const modules = Object.keys({
+  ...pak.peerDependencies,
+});
+
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+ const config = {
+  projectRoot: __dirname,
+  watchFolders: [root],
+
+  resolver: {
+    blacklistRE: exclusionList(
+      modules.map(
+        (m) =>
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      )
+    ),
+
+    extraNodeModules: modules.reduce((acc, name) => {
+      acc[name] = path.join(__dirname, 'node_modules', name);
+      return acc;
+    }, {}),
+  },
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
